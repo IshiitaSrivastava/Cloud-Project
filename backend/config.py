67131d7b26d74dac47f1da94ff2bfff3e38bd070
IshiitaSrivastava <ishitasrivastava421@gmail.com>
2025-11-19 21:03:44 +0530

    Update config.py

--- backend/config.py
+++ backend/config.py
@@ -1,1 +1,11 @@
-print('config')
+# backend/config.py
+import os
+from dotenv import load_dotenv
+
+basedir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+load_dotenv(dotenv_path=os.path.join(basedir, '.env'))
+
+class Config:
+    SECRET_KEY = os.getenv('SECRET_KEY', 'dev-secret')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'sqlite:///../voting.db')
+    JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY', 'jwt-secret')
