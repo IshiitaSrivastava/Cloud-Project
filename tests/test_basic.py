708d6af115fee51c739b753d6135b38ad4f9f561
IshiitaSrivastava <ishitasrivastava421@gmail.com>
2025-11-19 21:12:21 +0530

    Update test_basic.py

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,1 +1,8 @@
-assert True
+# tests/test_basic.py
+import requests
+
+BASE = 'http://localhost:5000/api'
+
+def test_list_elections():
+    r = requests.get(BASE + '/elections')
+    assert r.status_code == 200
